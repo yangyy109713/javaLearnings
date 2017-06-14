418b91879069429b30aad99c69ffbc3695e09ea0
yuanyuanyang1 <yuanyuanyang1@WSWINCNHZ1658.hengtiansoft.com>
2017-06-14 09:58:36 +0800

    update

--- src/webdriver/cookie/CookieTest.java
+++ src/webdriver/cookie/CookieTest.java
@@ -77,7 +77,7 @@ public class CookieTest implements Serializable{
 
 	@Test
 	public void saveCookieTest() throws FileNotFoundException, InterruptedException, IOException{
-		//saveCookieNew("yangyy","123456","cookie.data");
+		//saveCookieNew("yangyy","123456","cookies.data");
 		getCookieLogin(homeUrl,cookiePath);
 	}
 	
@@ -102,12 +102,6 @@ public class CookieTest implements Serializable{
 					
 					//System.out.println("path:"+path);//for test
 					Date expiry = null;//cookie的失效时间，默认存在浏览器打开期间
-					String expiryString = st.nextToken();
-					/*if(!(expiryString.equals(null)&& expiryString.equals("false")))
-                    {
-						expiry = (Date)st.nextElement();//编译报错java.lang.String cannot be cast to java.util.Date
-						System.out.println("expiry:"+expiryString);//for test
-                    }*/
 					boolean isSecure = new Boolean(st.nextToken()).booleanValue();
 					//System.out.println("isSecure:"+isSecure);//for test
 					
@@ -151,7 +145,7 @@ public class CookieTest implements Serializable{
 		Set<Cookie> cookies = driver.manage().getCookies();
 		System.out.println("cookie_size:"+cookies.size());
 		File file = new File(cookiepath);
-		file.delete();
+		file.delete();//if there is same file,delete it
 		try {
 			file.createNewFile();
 			FileWriter fw = new FileWriter(file);
@@ -163,11 +157,9 @@ public class CookieTest implements Serializable{
 			bw.flush();
 			bw.close();
 			fw.close();
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
 	
 	/**
@@ -210,99 +202,6 @@ public class CookieTest implements Serializable{
 		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File(savecookiepath)));
 		oos.writeObject(cookieStore);
 		oos.close();
-	}
-	
-	public void testIsbn() throws Exception {
-		driver.get(baseUrl + "ExerciseBookManager/web/auth/login");
-		Thread.sleep(1000);
-		// driver.manage().window().maximize();//窗口最大化
-		driver.findElement(By.id("userName")).clear();
-		driver.findElement(By.id("userName")).sendKeys("yangyy");
-		Thread.sleep(1000);
-		driver.findElement(By.id("password")).clear();
-		driver.findElement(By.id("password")).sendKeys("123456");
-		Thread.sleep(1000);
-		driver.findElement(By.xpath("//button[@type='button']")).click();
-		Thread.sleep(1000);
-		driver.findElement(By.linkText("ISBN管理")).click();
-		Thread.sleep(1000);
-		driver.findElement(
-				By.xpath("//div[@id='main-menu-inner']/ul/li/ul/li/a/span"))
-				.click();
-		Thread.sleep(1000);
-		driver.findElement(By.id("barcode")).clear();
-		driver.findElement(By.id("barcode")).sendKeys("7131091234570");
-		driver.findElement(By.id("barcode")).sendKeys(Keys.TAB);
-		Thread.sleep(1000);
-		driver.findElement(By.id("search-btn")).click();
-		Thread.sleep(10000);
-		driver.findElement(By.linkText("新建作业本")).click();
-		Thread.sleep(10000);
-		assertEquals("确定需要新建作业本？", closeAlertAndGetItsText());
-		driver.findElement(By.linkText("编辑")).click();
-		List<String> list = new ArrayList<String>(driver.getWindowHandles());
-		Thread.sleep(1000);
-		driver.switchTo().window(list.get(1));
-		Thread.sleep(1000);
-		driver.findElement(By.name("name")).clear();
-		driver.findElement(By.name("name")).sendKeys("樱桃之书");
-		new Select(driver.findElement(By.name("type")))
-				.selectByVisibleText("暑期作业");
-		new Select(driver.findElement(By.name("subject")))
-				.selectByVisibleText("科学");
-		new Select(driver.findElement(By.name("grade")))
-				.selectByVisibleText("三年级");
-		new Select(driver.findElement(By.name("term")))
-				.selectByVisibleText("上册");
-		new Select(driver.findElement(By.name("edition")))
-				.selectByVisibleText("苏教版");
-		driver.findElement(By.name("press")).clear();
-		driver.findElement(By.name("press")).sendKeys("出版社1");
-		driver.findElement(By.name("year")).clear();
-		driver.findElement(By.name("year")).sendKeys("2017");
-		driver.findElement(By.name("month")).clear();
-		driver.findElement(By.name("month")).sendKeys("05");
-		driver.findElement(By.name("revision")).clear();
-		driver.findElement(By.name("revision")).sendKeys("1");
-		driver.findElement(By.name("printYear")).clear();
-		driver.findElement(By.name("printYear")).sendKeys("2017");
-		driver.findElement(By.name("printMonth")).clear();
-		driver.findElement(By.name("printMonth")).sendKeys("05");
-		driver.findElement(By.name("printRevision")).clear();
-		driver.findElement(By.name("printRevision")).sendKeys("1");
-		driver.findElement(By.id("save-info-67669")).click();
-		assertEquals("你确定需要保存数据？", closeAlertAndGetItsText());
-		driver.findElement(By.id("cover-file-input-0")).clear();
-		driver.findElement(By.id("cover-file-input-0")).sendKeys(
-				"F:\\FastStone\\00.jpg");
-		new Select(driver.findElement(By.name("hasAnswer")))
-				.selectByVisibleText("否");
-		driver.findElement(By.id("save-info-67669")).click();
-		assertEquals("你确定需要保存数据？", closeAlertAndGetItsText());
-		driver.findElement(By.id("search-btn")).click();
-		driver.findElement(By.linkText("处理")).click();
-		new Select(driver.findElement(By.id("workflowOp")))
-				.selectByVisibleText("已购买");
-		driver.findElement(By.cssSelector("option[value=\"100200100000\"]"))
-				.click();
-		new Select(driver.findElement(By.id("nextUserId")))
-				.selectByVisibleText("yangyyhandle");
-		new Select(driver.findElement(By.name("workflowOpTreatmentId")))
-				.selectByVisibleText("购买含答案纸质书");
-		driver.findElement(By.xpath("(//button[@type='button'])[15]")).click();
-		assertEquals("您确定需要提交该处理？", closeAlertAndGetItsText());
-		driver.findElement(By.cssSelector("span.glyphicon.glyphicon-cog"))
-				.click();
-		new Select(driver.findElement(By.id("workflowOp")))
-				.selectByVisibleText("已到货");
-		new Select(driver.findElement(By.id("nextUserId")))
-				.selectByVisibleText("yangyyhandle");
-		driver.findElement(By.xpath("//button[@value='']")).click();
-		assertEquals("您确定需要提交该处理？", closeAlertAndGetItsText());
-		driver.findElement(By.xpath("//button[@value='']")).click();
-		assertEquals("您确定需要提交该处理？", closeAlertAndGetItsText());
-		driver.findElement(By.xpath("//button[@value='']")).click();
-		assertEquals("您确定需要提交该处理？", closeAlertAndGetItsText());
 	}
 
 	@After
