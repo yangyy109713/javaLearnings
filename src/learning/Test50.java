e9a7be7bd351d5298c4e92bcf62fcb94fab3f43e
yuanyuanyang1 <yuanyuanyang1@hengtiansoft.com>
2017-08-24 11:00:52 +0800

    add

--- src/learning/Test50.java
+++ src/learning/Test50.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 /**
  * Created by yuanyuanyang1 on 8/23/2017.
  * 题目：有五个学生，每个学生有3门课的成绩，从键盘输入以上数据（包括学生号，姓名，三门课成绩），
- * 计算出平均成绩，把原有的数据和计算出的平均分数存放在磁盘文件 "stud "中。
+ * 计算出平均成绩，把原有的数据和计算出的平均分数存放在磁盘文件 "stud.txt"中。
  */
 public class Test50 {
     public static void main(String[] args){
