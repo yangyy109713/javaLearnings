42c3a1e6931b9fc5a71a84ecf47b124883564868
yuanyuanyang1 <yuanyuanyang1@hengtiansoft.com>
2017-09-11 18:15:21 +0800

    add

--- src/interfaceCoding/InterfaceDemo.java
+++ src/interfaceCoding/InterfaceDemo.java
@@ -4,10 +4,13 @@ package interfaceCoding;
  * Created by yuanyuanyang1 on 9/11/2017.
  */
 public class InterfaceDemo {
-
+    public static void main(String[] args){
+        IUSB iu = new Printer();
+        iu.work();
+    }
 }
 
-interface iUSB{
+interface IUSB{
     void work();
 }
 
@@ -15,19 +18,19 @@ class Device{
 
 }
 
-class Printer extends Device implements iUSB{
+class Printer extends Device implements IUSB{
     public void work(){
         System.out.println("printer is printing...");
     }
 }
 
-class Usb extends Device implements iUSB{
+class Usb extends Device implements IUSB{
     public void work(){
         System.out.println("Usb is working...");
     }
 }
 
-class KeyBoard extends Device implements iUSB{
+class KeyBoard extends Device implements IUSB{
     public void work(){
         System.out.println("keyBoard down and up...");
     }
