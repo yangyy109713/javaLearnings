78ab81c9ac0fb330c5335bb85680adb8c444dce2
yangyy109713 <1175673598@qq.com>
2017-09-19 21:28:15 +0800

    add

--- src/interfaceCoding/InterfaceDemo.java
+++ src/interfaceCoding/InterfaceDemo.java
@@ -2,36 +2,75 @@ package interfaceCoding;
 
 /**
  * Created by yuanyuanyang1 on 9/11/2017.
+ * 使用USB设备来工作－－面向接口编程学习
+ * 多态的好处：把实现类对象赋给接口类型变量，屏蔽了不同实现类之间的差异，从而可以做到通用编程。
  */
 public class InterfaceDemo {
     public static void main(String[] args){
-        IUSB iu = new Printer();
-        iu.work();
+        Mouse m = new Mouse();
+        MotherBoard.pluginIn(m);
+
+        Printer p = new Printer();
+        MotherBoard.pluginIn(p);
+
+        MotherBoard.pluginIn(new Mouse());
+        MotherBoard.pluginIn(new Mouse());
+        MotherBoard.pluginIn(new Mouse());
+        MotherBoard.pluginIn(new Mouse());
+        MotherBoard.pluginIn(new Mouse());
+        MotherBoard.pluginIn(new Mouse());
+
+        MotherBoard.work();
     }
 }
 
-interface IUSB{
-    void work();
+interface  IUSB {
+    void swapData();
 }
 
-class Device{
-
-}
-
-class Printer extends Device implements IUSB{
-    public void work(){
-        System.out.println("printer is printing...");
+class Mouse implements IUSB {
+    public void swapData(){
+        System.out.println("鼠标ing...");
     }
 }
 
-class Usb extends Device implements IUSB{
-    public void work(){
-        System.out.println("Usb is working...");
+class Printer implements IUSB {
+    public void swapData(){
+        System.out.println("打印机ing");
     }
 }
 
-class KeyBoard extends Device implements IUSB{
-    public void work(){
-        System.out.println("keyBoard down and up...");
+class Usb {
+
+}
+
+//主板，又叫母板  鼠标接入主板才能工作
+class MotherBoard{
+    /*
+    如果再有打印机或USB等设备需要接入，需要再定义方法，传入打印机或USB等对象
+    public static void pluginIn(Mouse m){
+        m.swapData();
+    }
+     */
+
+    private static IUSB[] iusbs = new IUSB[6];//最多6个接口
+    private static int i = 0;//插入到第几个位置
+
+    //面向接口编程
+    public static void pluginIn(IUSB iusb){
+        if(i == iusbs.length){
+            System.out.println("插满了ing...");
+            return;
+        }
+        iusbs[i] = iusb;
+        i ++;
+    }
+
+    public static void work(){
+        for(IUSB i : iusbs){
+            if(i != null){
+                i.swapData();
+            }
+        }
     }
 }
